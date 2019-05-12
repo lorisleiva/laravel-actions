df5a547db1b0e559ae94678ebb297c2392de9ec4
Loris Leiva <loris.leiva@gmail.com>
2019-05-12 12:56:58 +0100

    :white_check_mark: Add tests for can helper method

--- tests/ResolvesAuthorizationTest.php
+++ tests/ResolvesAuthorizationTest.php
@@ -2,6 +2,8 @@
 
 namespace Lorisleiva\Actions\Tests;
 
+use Illuminate\Support\Facades\Gate;
+use Lorisleiva\Actions\Tests\Stubs\User;
 use Illuminate\Auth\Access\AuthorizationException;
 use Lorisleiva\Actions\Tests\Actions\SimpleCalculator;
 
@@ -39,5 +41,21 @@ class ResolvesAuthorizationTest extends TestCase
 
         $this->assertFalse($action->passesAuthorization());
         $action->run();
+    }
+
+    /** @test */
+    public function it_provides_a_shortcut_for_gate_checks()
+    {
+        Gate::define('perform-calculation', function (?User $user, $operation) {
+            return $operation === 'addition';
+        });
+
+        $action = new class(['operation' => 'addition']) extends SimpleCalculator {
+            public function authorize() {
+                return $this->can('perform-calculation', $this->operation);
+            }
+        };
+
+        $this->assertTrue($action->passesAuthorization());
     }
 }
