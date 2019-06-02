b2b3600d3c534b3a5c9f8fd23a1996bc0d698f3c
Loris Leiva <loris.leiva@gmail.com>
2019-06-02 23:29:13 +0100

    :recycle: Decorate event dispatcher

---
+++ src/EventDispatcherDecorator.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace LorisLeiva\Actions;
+
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Contracts\Container\Container as ContainerContract;
+
+class EventDispatcherDecorator implements DispatcherContract
+{
+    protected $dispatcher;
+    protected $container;
+
+    public function __construct(DispatcherContract $dispatcher, ContainerContract $container)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->container = $container;
+    }
+
+    public function listen($events, $listener)
+    {
+        if ($this->isActionFullyQualifiedName($listener)) {
+            $listener = $listener . '@runAsListener';
+        }
+
+        return $this->dispatcher->listen($events, $listener);
+    }
+
+    public function isActionFullyQualifiedName($listener)
+    {
+        if (! is_string($listener)) {
+            return false;
+        }
+
+        [$class, $method] = Str::parseCallback($listener);
+
+        if (! is_null($method)) {
+            return false;
+        }
+
+        return $this->container->make($class) instanceof Action;
+    }
+    
+    public function hasListeners($eventName)
+    {
+        return $this->dispatcher->hasListeners($eventName);
+    }
+    
+    public function subscribe($subscriber)
+    {
+        return $this->dispatcher->subscribe($subscriber);
+    }
+    
+    public function until($event, $payload = [])
+    {
+        return $this->dispatcher->until($event, $payload);
+    }
+    
+    public function dispatch($event, $payload = [], $halt = false)
+    {
+        return $this->dispatcher->dispatch($event, $payload, $halt);
+    }
+    
+    public function push($event, $payload = [])
+    {
+        return $this->dispatcher->push($event, $payload);
+    }
+    
+    public function flush($event)
+    {
+        return $this->dispatcher->flush($event);
+    }
+    
+    public function forget($event)
+    {
+        return $this->dispatcher->forget($event);
+    }
+    
+    public function forgetPushed()
+    {
+        return $this->dispatcher->forgetPushed();
+    }
+
+    public function __call($method, $parameters)
+    {
+        return $this->dispatcher->{$method}(...$parameters);
+    }
+}
