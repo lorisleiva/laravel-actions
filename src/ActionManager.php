fb7e55bf28c17807b31feeeab1abf08c800ab0e5
Loris Leiva <loris.leiva@gmail.com>
2020-04-12 17:21:44 +0100

    Polish the ActionManager

--- src/ActionManager.php
+++ src/ActionManager.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Str;
 use ReflectionClass;
+use ReflectionException;
 use Symfony\Component\Finder\Finder;
 
 class ActionManager
@@ -57,10 +58,11 @@ class ActionManager
      * Load one action either through an object or its classname.
      *
      * @param Action|string $action
+     * @throws ReflectionException
      */
     public function loadAction($action): void
     {
-        if (is_string($action) && ! $this->isActionClassname($action)) {
+        if (! $this->isAction($action)) {
             return;
         }
 
@@ -71,7 +73,13 @@ class ActionManager
         $action->registerCommand();
     }
 
-    protected function getClassnameFromPathname($pathname): string
+    /**
+     * Get the fully-qualified name of a class from its pathname.
+     *
+     * @param string $pathname
+     * @return string
+     */
+    protected function getClassnameFromPathname(string $pathname): string
     {
         return App::getNamespace() . str_replace(
             ['/', '.php'],
@@ -80,7 +88,14 @@ class ActionManager
         );
     }
 
-    protected function isActionClassname($action): bool
+    /**
+     * Determine if an object or its classname is an Action.
+     *
+     * @param mixed $action
+     * @return bool
+     * @throws ReflectionException
+     */
+    protected function isAction($action): bool
     {
         return is_subclass_of($action, Action::class) &&
             ! (new ReflectionClass($action))->isAbstract();
