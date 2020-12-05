038c51ab1aa963587264fac769d9ec4cf1e8c055
Loris Leiva <loris.leiva@gmail.com>
2020-12-05 12:48:51 +0000

    wip

--- tests/AsJobTest.php
+++ tests/AsJobTest.php
@@ -4,7 +4,6 @@ namespace Lorisleiva\Actions\Tests;
 
 use Carbon\Carbon;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Foundation\Bus\PendingDispatch;
 use Illuminate\Support\Facades\Queue;
 use Lorisleiva\Actions\Concerns\AsJob;
 use Lorisleiva\Actions\Decorators\JobDecorator;
@@ -13,22 +12,40 @@ class AsJobTest
 {
     use AsJob;
 
+    /** @var int */
+    public static $constructed = 0;
+
+    /** @var int */
+    public static $handled = 0;
+
+    public function __construct()
+    {
+        static::$constructed++;
+    }
+
     public function handle()
     {
-        //
+        static::$handled++;
     }
 }
 
 beforeEach(function () {
     // Given we mock the queue driver.
     Queue::fake();
+
+    // And reset the static counters.
+    AsJobTest::$constructed = 0;
+    AsJobTest::$handled = 0;
 });
 
 it('can be dispatched synchronously', function () {
-    // When we dispatch the job now.
+    // When we dispatch a job now.
     AsJobTest::dispatchNow();
 
-    // Then it was pushed to the queue using the "sync" connection.
+    // Then the handle method executed.
+    expect(AsJobTest::$handled)->toBe(1);
+
+    // And it was pushed to the queue using the "sync" connection.
     assertJobPushed(AsJobTest::class, function (JobDecorator $job) {
         return $job->connection === 'sync';
     });
@@ -38,7 +55,7 @@ it('can be dispatched synchronously with parameters', function () {
     // Given the following job parameters.
     $parameters = [1, 'two', new Filesystem()];
 
-    // When we dispatch the job now with these parameters.
+    // When we dispatch a job now with these parameters.
     AsJobTest::dispatchNow(...$parameters);
 
     // Then it was pushed to the queue with these parameters.
@@ -46,7 +63,7 @@ it('can be dispatched synchronously with parameters', function () {
 });
 
 it('can be dispatched asynchronously', function () {
-    // When we dispatch the job asynchronously.
+    // When we dispatch a job asynchronously.
     AsJobTest::dispatch();
 
     // Then it was pushed to the queue using the default connection.
@@ -59,7 +76,7 @@ it('can be dispatched asynchronously with parameters', function () {
     // Given the following job parameters.
     $parameters = [1, 'two', new Filesystem()];
 
-    // When we dispatch the job now with these parameters.
+    // When we dispatch a job now with these parameters.
     AsJobTest::dispatch(...$parameters);
 
     // Then it was pushed to the queue with these parameters.
@@ -67,7 +84,7 @@ it('can be dispatched asynchronously with parameters', function () {
 });
 
 it('can be dispatched with overridden configurations', function () {
-    // When we dispatch the job with the following configurations.
+    // When we dispatch a job with the following configurations.
     AsJobTest::dispatch()
         ->onConnection('my_connection')
         ->onQueue('my_queue')
@@ -82,10 +99,34 @@ it('can be dispatched with overridden configurations', function () {
 });
 
 it('can be dispatched after the response', function () {
-    // When we dispatch the job after the response.
-    $pendingJob = AsJobTest::dispatchAfterResponse();
+    // When we dispatch a job after the response.
+    AsJobTest::dispatchAfterResponse();
 
-    expect($pendingJob)->toBeInstanceOf(PendingDispatch::class);
+    // Then it is not dispatch immediately.
+    expect(AsJobTest::$handled)->toBe(0);
+
+    // But when the app terminates.
+    app()->terminate();
+
+    // Then the job was dispatched.
+    expect(AsJobTest::$handled)->toBe(1);
 });
 
-it('uses the decorated action as display name')->skip();
+it('constructs a new job at every dispatch', function () {
+    // When we dispatch a job twice.
+    AsJobTest::dispatchNow();
+    AsJobTest::dispatchNow();
+
+    // It has been initialised twice.
+    expect(AsJobTest::$constructed)->toBe(2);
+});
+
+it('uses the decorated action as display name by default', function () {
+    // When we dispatch a job.
+    AsJobTest::dispatch();
+
+    // Then its default display name is the classname of the decorated action.
+    assertJobPushed(AsJobTest::class, function (JobDecorator $job) {
+        return $job->displayName() === AsJobTest::class;
+    });
+});
