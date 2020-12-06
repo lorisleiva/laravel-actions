e0143615f00f52f20696983b0389104e1a6bf075
Loris Leiva <loris.leiva@gmail.com>
2020-12-06 12:52:48 +0000

    wip

--- tests/AsJobSerializedTest.php
+++ tests/AsJobSerializedTest.php
@@ -2,6 +2,7 @@
 
 namespace Lorisleiva\Actions\Tests;
 
+use Illuminate\Contracts\Database\ModelIdentifier;
 use Illuminate\Database\Eloquent\Collection;
 use Lorisleiva\Actions\Concerns\AsJob;
 use Lorisleiva\Actions\Decorators\JobDecorator;
@@ -17,7 +18,7 @@ class AsJobSerializedTest
 }
 
 it('serialises the action into its classname', function () {
-    // When we serialise the JobDecorator.
+    // When we serialise a JobDecorator.
     $job = AsJobSerializedTest::makeJob();
     $serializedJob = parseSerializedData(serialize($job));
 
@@ -48,12 +49,35 @@ it('serialises Eloquent models within the parameters', function () {
     // Given an persisted model.
     loadMigrations();
     $model = createUser();
+
+    // When we serialise a JobDecorator that has a model as a parameter.
+    $job = AsJobSerializedTest::makeJob($model);
+    $serializedJob = parseSerializedData(serialize($job));
+
+    // Then the model parameter has been serialised into a ModelIdentifier.
+    $firstParameter = (array) data_get($serializedJob, 'parameters.0');
+    expect($firstParameter)->toBe([
+        '__PHP_Incomplete_Class_Name' => ModelIdentifier::class,
+        'class' => get_class($model),
+        'id' => $model->id,
+        'relations' => [],
+        'connection' => 'sqlite',
+    ]);
 });
 
 it('unserialises Eloquent models within the parameters', function () {
     // Given an persisted model.
     loadMigrations();
     $model = createUser();
+
+    // When we serialise and unserialise a JobDecorator that has a model as a parameter.
+    $job = AsJobSerializedTest::makeJob($model);
+    $unserializedJob = unserialize(serialize($job));
+
+    // Then the model parameter has been unserialised to a new model instance.
+    $firstParameter = $unserializedJob->getParameters()[0];
+    expect($firstParameter)->toBeInstanceOf(get_class($model));
+    expect($firstParameter->id)->toBe($model->id);
 });
 
 it('serialises Eloquent collections within the parameters', function () {
@@ -62,6 +86,20 @@ it('serialises Eloquent collections within the parameters', function () {
     $modelA = createUser();
     $modelB = createUser();
     $collection = Collection::make([$modelA, $modelB]);
+
+    // When we serialise a JobDecorator that has a collection of models as a parameter.
+    $job = AsJobSerializedTest::makeJob($collection);
+    $serializedJob = parseSerializedData(serialize($job));
+
+    // Then the collection parameter has been serialised into a ModelIdentifier.
+    $firstParameter = (array) data_get($serializedJob, 'parameters.0');
+    expect($firstParameter)->toBe([
+        '__PHP_Incomplete_Class_Name' => ModelIdentifier::class,
+        'class' => get_class($modelA),
+        'id' => $collection->pluck('id')->toArray(),
+        'relations' => [],
+        'connection' => 'sqlite',
+    ]);
 });
 
 it('unserialises Eloquent collections within the parameters', function () {
@@ -70,4 +108,16 @@ it('unserialises Eloquent collections within the parameters', function () {
     $modelA = createUser();
     $modelB = createUser();
     $collection = Collection::make([$modelA, $modelB]);
+
+    // When we serialise and unserialise a JobDecorator that has a collection of models as a parameter.
+    $job = AsJobSerializedTest::makeJob($collection);
+    $unserializedJob = unserialize(serialize($job));
+
+    // Then the collection parameter has been unserialised to a new collection of models.
+    $firstParameter = $unserializedJob->getParameters()[0];
+    expect($firstParameter)->toBeInstanceOf(Collection::class);
+    expect($firstParameter->get(0))->toBeInstanceOf(get_class($modelA));
+    expect($firstParameter->get(0)->id)->toBe($modelA->id);
+    expect($firstParameter->get(1))->toBeInstanceOf(get_class($modelB));
+    expect($firstParameter->get(1)->id)->toBe($modelB->id);
 });
