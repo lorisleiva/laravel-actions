4d0893ec5cf95643bae0f62b02616f0985e9fa78
Loris Leiva <loris.leiva@gmail.com>
2020-12-07 09:22:20 +0000

    wip

---
+++ tests/AsControllerWithPolicyResponsesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lorisleiva\Actions\Tests;
+
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Route;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsController;
+
+class AsControllerWithPolicyResponsesTest
+{
+    use AsController;
+
+    public function authorize(ActionRequest $request): Response
+    {
+        if ($request->get('operation') === 'unauthorized') {
+            return Response::deny('My custom authorization message.', 418);
+        }
+
+        return Response::allow();
+    }
+
+    public function handle()
+    {
+        //
+    }
+}
+
+beforeEach(function () {
+    // Given the action returning policy responses is registered as a controller.
+    Route::post('/controller', AsControllerWithPolicyResponsesTest::class);
+});
+
+it('passes authorization', function () {
+    // When we make an authorised request.
+    $reponse = $this->postJson('/controller');
+
+    // Then we receive a successful response.
+    $reponse->assertOk();
+});
+
+it('fails authorization', function () {
+    // When we make an unauthorised request.
+    $reponse = $this->postJson('/controller', [
+        'operation' => 'unauthorized',
+    ]);
+
+    // Then we receive a forbidden error with our custom message and status.
+    $reponse->assertStatus(418);
+    $reponse->assertExactJson([
+        'message' => 'My custom authorization message.',
+    ]);
+});
