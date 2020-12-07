77ed281715b6cd0cd1cf51a48337a27e9a57a11a
Loris Leiva <loris.leiva@gmail.com>
2020-12-07 13:39:12 +0000

    wip

--- src/Concerns/AsFake.php
+++ src/Concerns/AsFake.php
@@ -4,54 +4,112 @@ namespace Lorisleiva\Actions\Concerns;
 
 use Mockery;
 use Mockery\Expectation;
+use Mockery\ExpectationInterface;
+use Mockery\HigherOrderMessage;
 use Mockery\MockInterface;
 
 trait AsFake
 {
-    protected static ?MockInterface $resolvedFakeInstance = null;
+    /** @var mixed|null */
+    protected static $originalResolvedInstance = null;
 
+    /** @var MockInterface|null  */
+    protected static ?MockInterface $fakeResolvedInstance = null;
+
+    /**
+     * @return MockInterface
+     */
     public static function mock(): MockInterface
     {
         if (static::isFake()) {
-            return static::$resolvedFakeInstance;
+            return static::$fakeResolvedInstance;
         }
 
         $mock = Mockery::mock(static::class);
         $mock->shouldAllowMockingProtectedMethods();
-        app()->instance(static::class, $mock);
 
-        return static::$resolvedFakeInstance = $mock;
+        return static::setFakeResolvedInstance($mock);
     }
 
+    /**
+     * @return MockInterface
+     */
     public static function spy(): MockInterface
     {
         if (static::isFake()) {
-            return static::$resolvedFakeInstance;
+            return static::$fakeResolvedInstance;
         }
 
-        $spy = Mockery::spy(static::class);
-        app()->instance(static::class, $spy);
-
-        return static::$resolvedFakeInstance = $spy;
+        return static::setFakeResolvedInstance(Mockery::spy(static::class));
     }
 
+    /**
+     * @return MockInterface
+     */
     public static function partialMock(): MockInterface
     {
         return static::mock()->makePartial();
     }
 
-    public static function shouldRun(): Expectation
+    /**
+     * @return Expectation|ExpectationInterface|HigherOrderMessage
+     */
+    public static function shouldRun()
     {
         return static::mock()->shouldReceive('handle');
     }
 
-    public static function shouldNotRun(): Expectation
+    /**
+     * @return Expectation|ExpectationInterface|HigherOrderMessage
+     */
+    public static function shouldNotRun()
     {
         return static::mock()->shouldNotReceive('handle');
     }
 
+    /**
+     * @return Expectation|ExpectationInterface|HigherOrderMessage|MockInterface
+     */
+    public static function allowToRun()
+    {
+        return static::spy()->allows('handle');
+    }
+
+    /**
+     * @return bool
+     */
     public static function isFake(): bool
     {
-        return ! is_null(static::$resolvedFakeInstance);
+        return ! is_null(static::$fakeResolvedInstance);
+    }
+
+    /**
+     * Removes the fake instance from the action.
+     */
+    public static function clearFake(): void
+    {
+        static::$fakeResolvedInstance = null;
+
+        if (is_null(static::$originalResolvedInstance)) {
+            app()->forgetInstance(static::class);
+        } else {
+            app()->instance(static::class, static::$originalResolvedInstance);
+            static::$originalResolvedInstance = null;
+        }
+    }
+
+    /**
+     * @param MockInterface $fake
+     * @return MockInterface
+     */
+    protected static function setFakeResolvedInstance(MockInterface $fake): MockInterface
+    {
+        if (app()->isShared(static::class)) {
+            static::$originalResolvedInstance = app(static::class);
+        }
+
+        app()->instance(static::class, $fake);
+
+        return static::$fakeResolvedInstance = $fake;
     }
 }
--- tests/AsFakeTest.php
+++ tests/AsFakeTest.php
@@ -4,10 +4,12 @@ namespace Lorisleiva\Actions\Tests;
 
 use Closure;
 use Lorisleiva\Actions\Concerns\AsFake;
+use Lorisleiva\Actions\Concerns\AsObject;
 use Mockery\MockInterface;
 
 class AsFakeTest
 {
+    use AsObject;
     use AsFake;
 
     public function handle(string $operation, int $left, int $right): int
@@ -33,6 +35,11 @@ class AsFakeTest
     }
 }
 
+beforeEach(function () {
+    // Given we clear the fake resolved instance between each test.
+    AsFakeTest::clearFake();
+});
+
 it('can mock an action', function () {
     // Given we create a mock from the action.
     $mock = AsFakeTest::mock();
@@ -45,6 +52,27 @@ it('can mock an action', function () {
     expect($resolved)->toBeInstanceOf(MockInterface::class);
 });
 
+it('can mock an action expecting for it to run', function () {
+    // Given we mock the action with some expectations.
+    AsFakeTest::shouldRun()
+        ->with('addition', 1, 2)
+        ->andReturn(3);
+
+    // When we run the action with the expected arguments
+    $result = AsFakeTest::run('addition', 1, 2);
+
+    // Then we receive the expected return value.
+    expect($result)->toBe(3);
+});
+
+it('can mock an action expecting for it not to run', function () {
+    //
+});
+
+it('can partionally mock an action', function () {
+    //
+});
+
 it('can spy an action', function () {
     // Given we create a spy from the action.
     $spy = AsFakeTest::spy();
@@ -55,4 +83,8 @@ it('can spy an action', function () {
     // Then the resolved instance is a spy.
     expect($resolved)->toBe($spy);
     expect($resolved)->toBeInstanceOf(MockInterface::class);
+});
+
+it('can spy an action allowing it to run the handle method', function () {
+    //
 });
