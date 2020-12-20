81be69dfacf20ee79f8dcc7108f97d8dbda50b14
Loris Leiva <loris.leiva@gmail.com>
2020-12-20 17:01:56 +0000

    wip

--- src/Concerns/AsJob.php
+++ src/Concerns/AsJob.php
@@ -2,12 +2,15 @@
 
 namespace Lorisleiva\Actions\Concerns;
 
+use Closure;
 use Illuminate\Contracts\Bus\Dispatcher;
 use Illuminate\Foundation\Bus\PendingDispatch;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Fluent;
 use Lorisleiva\Actions\ActionPendingChain;
 use Lorisleiva\Actions\Decorators\JobDecorator;
 use Lorisleiva\Actions\Decorators\UniqueJobDecorator;
+use PHPUnit\Framework\Assert as PHPUnit;
 
 trait AsJob
 {
@@ -105,5 +108,77 @@ trait AsJob
     public static function withChain($chain): ActionPendingChain
     {
         return new ActionPendingChain(static::class, $chain);
+    }
+
+    /**
+     * @param Closure|int|null $times
+     * @param Closure|null $callback
+     */
+    public static function assertPushed($times = null, Closure $callback = null): void
+    {
+        if ($times instanceof Closure) {
+            $callback = $times;
+            $times = null;
+        }
+
+        $count = Queue::pushed(JobDecorator::class, function (JobDecorator $job, $queue) use ($callback) {
+            if (! $job->decorates(static::class)) {
+                return false;
+            }
+
+            if (! $callback) {
+                return true;
+            }
+
+            return $callback($job->getAction(), $job, $queue);
+        })->count();
+
+        $job = static::class;
+
+        if (is_null($times)) {
+            PHPUnit::assertTrue(
+                $count > 0,
+                "The expected [{$job}] job was not pushed."
+            );
+        } elseif ($times === 0) {
+            PHPUnit::assertTrue(
+                $count === 0,
+                "The unexpected [{$job}] job was pushed."
+            );
+        } else {
+            PHPUnit::assertSame(
+                $times, $count,
+                "The expected [{$job}] job was pushed {$count} times instead of {$times} times."
+            );
+        }
+    }
+
+    /**
+     * @param Closure|null $callback
+     */
+    public static function assertNotPushed(Closure $callback = null): void
+    {
+        static::assertPushed(0, $callback);
+    }
+
+    /**
+     * @param string $queue
+     * @param Closure|int|null $times
+     * @param Closure|null $callback
+     */
+    public static function assertPushedOn(string $queue, $times = null, Closure $callback = null): void
+    {
+        if ($times instanceof Closure) {
+            $callback = $times;
+            $times = null;
+        }
+
+        static::assertPushed($times, function ($action, $job, $pushedQueue) use ($callback, $queue) {
+            if ($pushedQueue !== $queue) {
+                return false;
+            }
+
+            return $callback ? $callback(...func_get_args()) : true;
+        });
     }
 }
---
+++ tests/AsJobWithAssertionsTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Lorisleiva\Actions\Tests;
+
+use Illuminate\Support\Facades\Queue;
+use Lorisleiva\Actions\Concerns\AsJob;
+use Lorisleiva\Actions\Decorators\JobDecorator;
+use PHPUnit\Framework\ExpectationFailedException;
+
+class AsJobWithAssertionsTest
+{
+    use AsJob;
+
+    public static ?string $queue;
+
+    public static function setQueue(?string $queue): void
+    {
+        self::$queue = $queue;
+    }
+
+    public function configureJob(JobDecorator $job)
+    {
+        $job->onQueue(static::$queue);
+    }
+
+    public function handle()
+    {
+        //
+    }
+}
+
+beforeEach(function () {
+    // Given we mock the queue driver.
+    Queue::fake();
+
+    // And reset the queue between each test.
+    AsJobWithAssertionsTest::$queue = null;
+});
+
+it('asserts an action has been pushed - success', function () {
+    // When we dispatch the action.
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we can assert it has been dispatched.
+    AsJobWithAssertionsTest::assertPushed();
+});
+
+it('asserts an action has been pushed - failure', function () {
+    // Given we don't dispatch the action.
+    // ...
+
+    // Then we fail the expectation.
+    $this->expectException(ExpectationFailedException::class);
+    $this->expectExceptionMessage('The expected ['.AsJobWithAssertionsTest::class.'] job was not pushed');
+
+    // When we assert that it was pushed.
+    AsJobWithAssertionsTest::assertPushed();
+});
+
+it('asserts an action has not been pushed - success', function () {
+    // When we don't dispatch the action.
+    // ...
+
+    // Then we can assert it has not been dispatched.
+    AsJobWithAssertionsTest::assertNotPushed();
+});
+
+it('asserts an action has not been pushed - failure', function () {
+    // Given we dispatched the action.
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we fail the expectation.
+    $this->expectException(ExpectationFailedException::class);
+    $this->expectExceptionMessage('The unexpected ['.AsJobWithAssertionsTest::class.'] job was pushed');
+
+    // When we assert that it was not pushed.
+    AsJobWithAssertionsTest::assertNotPushed();
+});
+
+it('asserts an action has been pushed a given amount of times - success', function () {
+    // When we dispatch the action twice.
+    AsJobWithAssertionsTest::dispatch();
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we can assert it has been dispatched.
+    AsJobWithAssertionsTest::assertPushed(2);
+});
+
+it('asserts an action has been pushed a given amount of times - failure', function () {
+    // Given we dispatched the action twice.
+    AsJobWithAssertionsTest::dispatch();
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we fail the expectation.
+    $this->expectException(ExpectationFailedException::class);
+    $this->expectExceptionMessage('The expected ['.AsJobWithAssertionsTest::class.'] job was pushed 2 times instead of 3 times');
+
+    // When we assert that it was pushed 3 times.
+    AsJobWithAssertionsTest::assertPushed(3);
+});
+
+it('asserts an action has been pushed on a given queue - success', function () {
+    // When we dispatch the action on "some-queue".
+    AsJobWithAssertionsTest::setQueue('some-queue');
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we can assert it has been dispatched on that queue.
+    AsJobWithAssertionsTest::assertPushedOn('some-queue');
+});
+
+it('asserts an action has been pushed on a given queue - failure', function () {
+    // Given we dispatched the action on "some-queue".
+    AsJobWithAssertionsTest::setQueue('some-queue');
+    AsJobWithAssertionsTest::dispatch();
+
+    // Then we fail the expectation.
+    $this->expectException(ExpectationFailedException::class);
+    $this->expectExceptionMessage('The expected ['.AsJobWithAssertionsTest::class.'] job was not pushed');
+
+    // When we pushed it on some other queue.
+    AsJobWithAssertionsTest::assertPushedOn('some-other-queue');
+});
