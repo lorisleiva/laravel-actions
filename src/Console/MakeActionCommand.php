51558870327b38154d5453de59d3a774f6059028
Craig Morris <craig.michael.morris@gmail.com>
2021-01-21 10:09:19 +0000

    rmdump

--- src/Console/MakeActionCommand.php
+++ src/Console/MakeActionCommand.php
@@ -25,7 +25,6 @@ class MakeActionCommand extends GeneratorCommand
      */
     protected function resolveStubPath($stub)
     {
-        dump($this->laravel->basePath(trim($stub, '/')));
         return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
             ? $customPath
             : __DIR__ . $stub;
