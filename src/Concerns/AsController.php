4ea0617f8c3f2964a5d14339946d593143170ae7
Loris Leiva <loris.leiva@gmail.com>
2022-01-30 11:05:39 +0000

    Remove temporary hack

--- src/Concerns/AsController.php
+++ src/Concerns/AsController.php
@@ -13,14 +13,4 @@ trait AsController
     {
         return $this->handle(...$arguments);
     }
-
-    /**
-     * Sadly necessary to trigger middleware resolution on the controller decorator since 9.x.
-     * @see https://github.com/laravel/framework/pull/40397#issuecomment-1022042544
-     * @return array
-     */
-    public function getMiddleware()
-    {
-        return [];
-    }
 }
