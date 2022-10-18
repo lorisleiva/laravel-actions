63235f3f204fdd22f430d4fa248fb167ac9e88bb
Loris Leiva <loris.leiva@gmail.com>
2022-10-18 18:24:55 +0100

    add more middleware tests

--- tests/AsControllerWithMiddlewareTest.php
+++ tests/AsControllerWithMiddlewareTest.php
@@ -11,10 +11,14 @@ class AsControllerWithMiddlewareTest
 {
     use AsController;
 
+    public static int $middlewareCounter = 0;
+
     public function getControllerMiddleware()
     {
         return [
             function (Request $request, $next) {
+                static::$middlewareCounter++;
+
                 if ($request->get('operation') === 'middleware') {
                     return response()->json(['caught by middleware']);
                 }
@@ -42,3 +46,47 @@ it('can register controller middleware', function () {
     // Then we receive a successful response.
     $response->assertOk()->assertExactJson(['caught by middleware']);
 });
+
+
+it('works with route middleware too', function () {
+    // Given the action is registered as a controller with a route middleware.
+    Route::post('/calculator', AsControllerWithMiddlewareTest::class)
+        ->middleware(RouteMiddleware::class);
+
+    // When we call that route.
+    $response = $this->postJson('/calculator', [
+        'operation' => 'route',
+    ]);
+
+    // Then we were intercepted by the route middleware.
+    $response->assertOk()->assertExactJson(['caught by route middleware']);
+});
+
+it('calls route and controller middleware exactly once', function () {
+    // Given the action is registered as a controller with a route middleware.
+    Route::post('/calculator', AsControllerWithMiddlewareTest::class)
+        ->middleware(RouteMiddleware::class);
+
+    // When we call that route.
+    $response = $this->postJson('/calculator');
+
+    // The we were intercepted by both the route and the controller middleware exactly one.
+    expect(RouteMiddleware::$counter)->toBe(1);
+    expect(AsControllerWithMiddlewareTest::$middlewareCounter)->toBe(1);
+});
+
+class RouteMiddleware
+{
+    public static int $counter = 0;
+
+    public function handle(Request $request, $next)
+    {
+        static::$counter++;
+
+        if ($request->get('operation') === 'route') {
+            return response()->json(['caught by route middleware']);
+        }
+
+        return $next($request);
+    }
+}
