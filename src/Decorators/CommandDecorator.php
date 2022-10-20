8da334ec6439052d9d22b44bb9a7738d4b8ff68b
lorisleiva <lorisleiva@users.noreply.github.com>
2022-10-20 17:53:54 +0000

    Fix styling

--- src/Decorators/CommandDecorator.php
+++ src/Decorators/CommandDecorator.php
@@ -2,8 +2,8 @@
 
 namespace Lorisleiva\Actions\Decorators;
 
-use Illuminate\Console\Command;
 use \Illuminate\Console\View\Components\Factory;
+use Illuminate\Console\Command;
 use Lorisleiva\Actions\Concerns\DecorateActions;
 use Lorisleiva\Actions\Exceptions\MissingCommandSignatureException;
 
