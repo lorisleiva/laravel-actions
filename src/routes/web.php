93ebd274b51baff9e1cae7799fed3d9db4158185
Tommi Carleman <me@tommicarleman.net>
2023-05-11 12:28:20 +0200

    forgot the web middleware from the route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
-Route::any('laravel-actions/{actionString}', \Lorisleiva\Actions\ActionController::class)->name('laravel-actions.route');
+Route::any('laravel-actions/{actionString}', \Lorisleiva\Actions\ActionController::class)
+    ->middleware('web')
+    ->name('laravel-actions.route');
