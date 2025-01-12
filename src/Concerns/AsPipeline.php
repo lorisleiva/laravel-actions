af9f9c58ee51d3ee5dec26d7c9b619bc02a42048
Steven Maguire <stevenmaguire@gmail.com>
2025-01-11 20:42:00 -0600

    Move pipeline closure resolution logic to decorator and expand test cases

--- src/Concerns/AsPipeline.php
+++ src/Concerns/AsPipeline.php
@@ -4,33 +4,5 @@ namespace Lorisleiva\Actions\Concerns;
 
 trait AsPipeline
 {
-    /**
-     * Typical pipeline behavior expects two things:
-     *
-     *     1)  The pipe class to expect a single incoming parameter (along with
-     *         a closure) and single return value.
-     *     2)  The pipe class to be aware of the next closure and determine what
-     *         should be passed into the next pipe.
-     *
-     * Because of these expectations, this behavior is asserting two opinions:
-     *
-     *     1)  Regardless of the number of parameters provided to the asPipeline
-     *         method implemented here, only the first will be supplied to the
-     *         invoked Action.
-     *     2)  If the invoked Action does not return anything, then the next
-     *         closure will be supplied the same parameter. However, if the
-     *         invoked action does return a non-null value, that value will
-     *         be supplied to the next closure.
-     *
-     * Also, this logic is implemented in the trait rather than the decorator
-     * to afford some flexibility to consuming projects, should the wish to
-     * implement their own logic in their Action classes directly.
-     */
-    public function asPipeline(mixed ...$arguments): mixed
-    {
-        $passable = array_shift($arguments);
-        $closure = array_pop($arguments);
-
-        return $closure($this->handle($passable) ?? $passable);
-    }
+    //
 }
--- tests/AsPipelineTest.php
+++ tests/AsPipelineTest.php
@@ -2,17 +2,39 @@
 
 namespace Lorisleiva\Actions\Tests;
 
+use ArgumentCountError;
+use Closure;
 use Illuminate\Support\Facades\Pipeline;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\AsPipeline;
+
+class AsPipelinePassable
+{
+    public function __construct(public int $count = 0)
+    {
+        //
+    }
+
+    public function increment(int $multiplier = 1)
+    {
+        $this->count = $this->count + (1 * $multiplier);
+    }
+}
 
 class AsPipelineExplicitTest
 {
     use AsPipeline;
 
-    public function handle($passable): void
+    public function handle(AsPipelinePassable $passable): void
     {
         $passable->increment();
+    }
+
+    public function asPipeline(AsPipelinePassable $passable): AsPipelinePassable
+    {
+        $this->handle($passable);
+
+        return $passable;
     }
 }
 
@@ -20,14 +42,63 @@ class AsPipelineImplicitTest
 {
     use AsAction;
 
-    public function handle($passable): void
+    public function handle(AsPipelinePassable $passable): void
+    {
+        $passable->increment();
+    }
+
+    public function asPipeline(AsPipelinePassable $passable): AsPipelinePassable
+    {
+        $this->handle($passable);
+
+        return $passable;
+    }
+}
+
+class AsPipelineMultipleParamTest
+{
+    use AsAction;
+
+    public function handle(AsPipelinePassable $passable): void
+    {
+        $passable->increment($multiplier);
+    }
+
+    public function asPipeline(AsPipelinePassable $passable, int $multiplier): AsPipelinePassable
+    {
+        $this->handle($passable);
+
+        return $passable;
+    }
+}
+
+class AsPipelineSingleParamHandleOnlyTest
+{
+    use AsAction;
+
+    public function handle(AsPipelinePassable $passable): void
     {
         $passable->increment();
     }
 }
 
+class AsPipelineMultipleParamHandleOnlyTest
+{
+    use AsAction;
+
+    public function handle(AsPipelinePassable $passable, int $multiplier): void
+    {
+        $passable->increment($multiplier);
+    }
+}
+
+class AsPipelineWithoutHandleOrAsPipeline
+{
+    use AsAction;
+}
+
 function getAnonymous() {
-    return function ($p, $next) {
+    return function (AsPipelinePassable $p, $next) {
         $p->increment();
 
         return $next($p);
@@ -35,17 +106,7 @@ function getAnonymous() {
 }
 
 function getPassable() {
-    return new class {
-        public function __construct(public int $count = 0)
-        {
-            //
-        }
-
-        public function increment()
-        {
-            $this->count++;
-        }
-    };
+    return new AsPipelinePassable;
 }
 
 it('can run as a pipe in a pipeline, with explicit trait', function () {
@@ -77,3 +138,79 @@ it('can run as a pipe in a pipeline, with implicit trait', function () {
     expect(is_object($passable))->toBe(true);
     expect($passable->count)->toBe(4);
 });
+
+it('can run as a pipe in a pipeline, without an explicit asPipeline method', function () {
+    $anonymous = getAnonymous();
+    $passable = Pipeline::send(getPassable())
+        ->through([
+            AsPipelineSingleParamHandleOnlyTest::class,
+            $anonymous,
+            AsPipelineSingleParamHandleOnlyTest::class,
+            $anonymous,
+        ])
+        ->thenReturn();
+
+    expect(is_object($passable))->toBe(true);
+    expect($passable->count)->toBe(4);
+});
+
+it('it can run as a noop/passthrough pipe in a pipeline, without a handle or asPipeline method', function () {
+    $anonymous = getAnonymous();
+    $passable = Pipeline::send(getPassable())
+        ->through([
+            AsPipelineWithoutHandleOrAsPipeline::class,
+            $anonymous,
+            AsPipelineWithoutHandleOrAsPipeline::class,
+            $anonymous,
+        ])
+        ->thenReturn();
+
+    expect(is_object($passable))->toBe(true);
+    expect($passable->count)->toBe(2);
+});
+
+it('it can run with an arbitrary via method configured on Pipeline', function () {
+    $anonymous = getAnonymous();
+    $passable = Pipeline::send(getPassable())
+        ->via('foobar')
+        ->through([
+            AsPipelineImplicitTest::class,
+            $anonymous,
+            AsPipelineImplicitTest::class,
+            $anonymous,
+        ])
+        ->thenReturn();
+
+    expect(is_object($passable))->toBe(true);
+    expect($passable->count)->toBe(4);
+});
+
+it('cannot run as a pipe in a pipeline, with an explicit asPipeline method expecting multiple non-optional params', function () {
+    $anonymous = getAnonymous();
+    $passable = Pipeline::send(getPassable())
+        ->through([
+            AsPipelineMultipleParamTest::class,
+            $anonymous,
+            AsPipelineMultipleParamTest::class,
+            $anonymous,
+        ])
+        ->thenReturn();
+
+    expect(is_object($passable))->toBe(true);
+    expect($passable->count)->toBe(10);
+})->throws(ArgumentCountError::class, 'Too few arguments to function Lorisleiva\Actions\Tests\AsPipelineMultipleParamTest::asPipeline(), 1 passed and exactly 2 expected');
+
+it('cannot run as a pipe in a pipeline, without an explicit asPipeline method and multiple non-optional handle params', function () {
+    $anonymous = getAnonymous();
+    $passable = Pipeline::send(getPassable())
+        ->through([
+            AsPipelineMultipleParamHandleOnlyTest::class,
+            $anonymous,
+            AsPipelineMultipleParamHandleOnlyTest::class,
+            $anonymous,
+        ])
+        ->thenReturn();
+
+    expect(is_object($passable))->toBe(true);
+    expect($passable->count)->toBe(10);
+})->throws(ArgumentCountError::class, 'Too few arguments to function Lorisleiva\Actions\Tests\AsPipelineMultipleParamHandleOnlyTest::handle(), 1 passed and exactly 2 expected');
