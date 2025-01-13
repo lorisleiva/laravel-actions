548aeced1d2fa849f61aa6692226ec799d75488a
Michiel Vugteveen <info@it-can.nl>
2025-01-13 20:11:46 +0100

    Fix php8.4 deprecated notices

--- src/Exceptions/MissingCommandSignatureException.php
+++ src/Exceptions/MissingCommandSignatureException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class MissingCommandSignatureException extends Exception
 {
-    public function __construct($action, $code = 0, Throwable $previous = null)
+    public function __construct($action, $code = 0, ?Throwable $previous = null)
     {
         $message = sprintf(
             'The command signature is missing from your [%s] action. Use `public string $commandSignature` to set it up.',
