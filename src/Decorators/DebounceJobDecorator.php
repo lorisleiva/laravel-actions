33baa9396986e99966c699721d3d0d94d107384c
Huy Tran <huy.t@greenhat.net>
2026-04-29 00:31:17 +1000

    Support Laravel 13.6 DebounceFor attribute. Actions annotated with #[DebounceFor] on the action class are now automatically wrapped in a DebounceJobDecorator, which passes Laravel's PendingDispatch attribute check and forwards the debounceFor value from the action's attribute to the job.

---
+++ src/Decorators/DebounceJobDecorator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lorisleiva\Actions\Decorators;
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Queue\Attributes\DebounceFor;
+use ReflectionClass;
+
+#[DebounceFor(0)]
+class DebounceJobDecorator extends JobDecorator
+{
+    public ?int $debounceFor = null;
+
+    protected function constructed(): void
+    {
+        $attributes = (new ReflectionClass($this->action))->getAttributes(DebounceFor::class);
+
+        if (! empty($attributes)) {
+            $this->debounceFor = $attributes[0]->newInstance()->debounceFor;
+        }
+
+        parent::constructed();
+    }
+
+    public function debounceVia()
+    {
+        return $this->fromActionMethod('getJobDebounceVia', $this->parameters, function () {
+            return Container::getInstance()->make(Cache::class);
+        });
+    }
+}
